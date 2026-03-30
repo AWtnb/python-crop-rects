24c1247549ae57207f917623c0987ba0ae8bf85a
AWtnb <46807238+AWtnb@users.noreply.github.com>
2026-03-30 15:26:40 +0900

    update

--- group.py
+++ group.py
@@ -2,6 +2,13 @@ import csv
 import os
 import sys
 from pathlib import Path
+from typing import NamedTuple
+
+
+class ExtractResult(NamedTuple):
+    base_file: str
+    is_title: bool
+    lines: list[str]
 
 
 def main(args: list[str]) -> None:
@@ -13,26 +20,38 @@ def main(args: list[str]) -> None:
     if not p.is_dir():
         return
 
-    d: dict[str, list[str]] = {}
+    d: dict[str, list[ExtractResult]] = {}
     for f in p.iterdir():
         if f.suffix != ".txt":
             continue
 
-        key = f.stem.split("_")[0]
         lines = [
             line for line in f.read_text("utf-8").strip().splitlines() if line.strip()
         ]
-        if key not in d:
-            d[key] = lines
+
+        base_name = f.stem.split("_")[0]
+        result = ExtractResult(base_name, f.stem.endswith("_commented"), lines)
+        if base_name not in d:
+            d[base_name] = [result]
         else:
-            d[key].extend(lines)
+            d[base_name].append(result)
 
     out_path = p / f"{list(d.keys())[0][:5]}.csv"
     with open(out_path, "w", encoding="utf-8", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow(["file", "text"])
-        for k, v in d.items():
-            writer.writerow([k, " ".join(v)])
+        writer.writerow(["file", "type", "text"])
+        for base_name, results in d.items():
+            title_lines = []
+            non_title_lines = []
+            for r in results:
+                if r.is_title:
+                    [title_lines.append(line) for line in r.lines]
+                else:
+                    [non_title_lines.append(line) for line in r.lines]
+            if len(title_lines) != 0:
+                writer.writerow([base_name, "タイトル", " ".join(title_lines)])
+            if len(non_title_lines) != 0:
+                writer.writerow([base_name, "本文", " ".join(non_title_lines)])
 
 
 if __name__ == "__main__":
